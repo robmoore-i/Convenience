fdb3bb01df10270a057152c23ba65c16b1f5bdbd
Rob <robmoore121@gmail.com>
2017-10-16 23:01:00 +0100

    Added some common monads

--- src/Conveniencer.java
+++ src/Conveniencer.java
@@ -2,6 +2,8 @@ import java.util.function.DoubleBinaryOperator;
 import java.util.function.DoubleUnaryOperator;
 
 public class Conveniencer {
+    // Dyads
+
     public Dyad add = Dyad.fromOperator((left, right) -> left + right);
     public Dyad times = Dyad.fromOperator((left, right) -> left * right);
     public Dyad minus = Dyad.fromOperator((left, right) -> left - right);
@@ -10,6 +12,12 @@ public class Conveniencer {
     public Dyad dyad(DoubleBinaryOperator operator) {
         return Dyad.fromOperator(operator);
     }
+
+    // Monads
+
+    public Monad neg = Monad.fromOperator(operand -> -operand);
+    public Monad reciprocal = Monad.fromOperator(operand -> 1 / operand);
+    public Monad sqrt = Monad.fromOperator(Math::sqrt);
 
     public Monad monad(DoubleUnaryOperator operator) {
         return Monad.fromOperator(operator);
