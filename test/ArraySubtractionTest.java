7b67b266cbf7f4eacd7eecc91fba483e53da564d
Rob <robmoore121@gmail.com>
2017-10-16 19:05:45 +0100

    Optimise imports

--- test/ArraySubtractionTest.java
+++ test/ArraySubtractionTest.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class ArraySubtractionTest {
