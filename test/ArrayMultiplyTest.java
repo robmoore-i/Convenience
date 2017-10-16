00f155404c9f08c2c7586d3bf475c15875669d95
Rob <robmoore121@gmail.com>
2017-10-16 18:52:50 +0100

    "multiply by" not "add to"

--- test/ArrayMultiplyTest.java
+++ test/ArrayMultiplyTest.java
@@ -8,22 +8,22 @@ public class ArrayMultiplyTest {
     private Conveniencer c = new Conveniencer();
 
     @Test
-    public void canAddScalarToScalar() {
+    public void canTimesScalarByScalar() {
         assertEquals(20, c.times(4, 5));
     }
 
     @Test
-    public void canAddScalarToVector() {
+    public void canTimesScalarByVector() {
         assertThat(c.times(5, new int[]{3, 4, 5}), equalTo(new int[]{15, 20, 25}));
     }
 
     @Test
-    public void canAddVectorToScalar() {
+    public void canTimesVectorByScalar() {
         assertThat(c.times(new int[]{0, 2, 6, 9, 4}, 9), equalTo(new int[]{0, 18, 54, 81, 36}));
     }
 
     @Test
-    public void canAddVectorToVector() throws LengthError {
+    public void canTimesVectorByVector() throws LengthError {
         assertThat(c.times(new int[]{0, 2, 6, 9, 4}, new int[]{1, 2, 3, 4, 5}), equalTo(new int[]{0, 4, 18, 36, 20}));
     }
 
