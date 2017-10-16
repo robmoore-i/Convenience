bc4c1ad5ded809921b414dc0edb13c6e7b127cae
Rob <robmoore121@gmail.com>
2017-10-16 18:28:34 +0100

    scalar + vector

--- src/Conveniencer.java
+++ src/Conveniencer.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.function.IntUnaryOperator;
+
 public class Conveniencer {
     public int add(int left, int right) {
         return left + right;
+    }
+
+    public int[] add(int left, int[] right) {
+        return Arrays.stream(right).map(operand -> left + operand).toArray();
     }
 }
--- test/ConveniencerTest.java
+++ test/ConveniencerTest.java
@@ -1,11 +1,19 @@
 import org.junit.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class ConveniencerTest {
+    private Conveniencer c = new Conveniencer();
+
     @Test
     public void canAddScalarToScalar() {
-        Conveniencer c = new Conveniencer();
         assertEquals(9, c.add(4, 5));
+    }
+
+    @Test
+    public void canAddScalarToVector() {
+        assertThat(c.add(5, new int[]{3, 4, 5}), equalTo(new int[]{8, 9, 10}));
     }
 }
