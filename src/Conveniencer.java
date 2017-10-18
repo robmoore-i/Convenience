198f99c3396430316c162706bdc13e81b7b42d2b
Rob <robmoore121@gmail.com>
2017-10-18 13:42:51 +0100

    reformat

--- src/Conveniencer.java
+++ src/Conveniencer.java
@@ -2,25 +2,23 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Conveniencer {
-    // Dyads
-
-    public Dyad<Double, Double, Double> add = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left + right);
-    public Dyad<Double, Double, Double> times = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left * right);
-    public Dyad<Double, Double, Double> minus = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left - right);
-    public Dyad<Double, Double, Double> divide = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left / right);
+    public <I, O> Monad<I, O> monad(Function<I, O> f) {
+        return Monad.fromOperator(f);
+    }
 
     public <L, R, O> Dyad<L, R, O> dyad(BiFunction<L, R, O> f) {
         return Dyad.fromOperator(f);
     }
 
-    // Monads
-
+    // MONADS //
     public Monad<Double, Double> neg = Monad.fromOperator((Function<Double, Double>) operand -> -operand);
     public Monad<Double, Double> reciprocal = Monad.fromOperator((Function<Double, Double>) operand -> 1 / operand);
     public Monad<Double, Double> sqrt = Monad.fromOperator(Math::sqrt);
     public Monad<Double, Double> abs = Monad.fromOperator((Function<Double, Double>) Math::abs);
 
-    public <I, O> Monad<I, O> monad(Function<I, O> f) {
-        return Monad.fromOperator(f);
-    }
+    // DYADS //
+    public Dyad<Double, Double, Double> add = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left + right);
+    public Dyad<Double, Double, Double> times = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left * right);
+    public Dyad<Double, Double, Double> minus = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left - right);
+    public Dyad<Double, Double, Double> divide = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left / right);
 }
