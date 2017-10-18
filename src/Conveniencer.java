0d99fd0d456f246df2cceeb10fe64464a95c0d52
Rob <robmoore121@gmail.com>
2017-10-18 16:58:39 +0100

    implemented take for positive and negative left arguments

--- src/Conveniencer.java
+++ src/Conveniencer.java
@@ -10,15 +10,51 @@ public class Conveniencer {
         return Dyad.fromOperator(f);
     }
 
-    // MONADS //
+    // NUMERIC MONADS //
     public Monad<Double, Double> neg = Monad.fromOperator((Function<Double, Double>) operand -> -operand);
     public Monad<Double, Double> reciprocal = Monad.fromOperator((Function<Double, Double>) operand -> 1 / operand);
     public Monad<Double, Double> sqrt = Monad.fromOperator(Math::sqrt);
+    public Monad<Double, Double> floor = Monad.fromOperator(Math::floor);
+    public Monad<Double, Double> ceiling = Monad.fromOperator(Math::ceil);
+    public Monad<Double, Double> log = Monad.fromOperator(Math::log);
+    public Monad<Double, Double> exp = Monad.fromOperator(Math::exp);
     public Monad<Double, Double> abs = Monad.fromOperator((Function<Double, Double>) Math::abs);
+    public Monad<Double, Double> signum = Monad.fromOperator((Function<Double, Double>) Math::signum);
 
-    // DYADS //
+    // NUMERIC DYADS //
     public Dyad<Double, Double, Double> add = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left + right);
     public Dyad<Double, Double, Double> times = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left * right);
     public Dyad<Double, Double, Double> minus = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left - right);
     public Dyad<Double, Double, Double> divide = Dyad.fromOperator((BiFunction<Double, Double, Double>) (left, right) -> left / right);
+
+    // STRING MONADS //
+
+    // STRING DYADS //
+
+    // GENERAL MONADS //
+
+    // GENERAL DYADS //
+    public <T> T[] take(int n, T[] a) {
+        int l = a.length;
+        int nAbs = Math.abs(n);
+        Object[] r = new Object[nAbs];
+        int ia = n > 0 ? 0 : l - 1;
+        int ir = n > 0 ? 0 : nAbs - 1;
+        int inc = n > 0 ? 1 : -1;
+        if (n > 0) {
+            while (ir < nAbs) {
+                r[ir] = a[ia % l];
+                ir += inc;
+                ia += inc;
+            }
+        } else {
+            while (ir >= 0) {
+                int mod = ia - l * Math.floorDiv(ia, l);
+                r[ir] = a[mod];
+                ir += inc;
+                ia += inc;
+            }
+        }
+        return (T[]) r;
+    }
 }
---
+++ test/ArrayTakeTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class ArrayTakeTest {
+    private Conveniencer c = new Conveniencer();
+    private Integer[] array = new Integer[]{1, 4, 9, 16, 25, 36, 49};
+
+    @Test
+    public void canTakeN() {
+        assertThat(c.take(3, array), equalTo(new Integer[]{1, 4, 9}));
+    }
+
+    @Test
+    public void canTakeNGreaterThanArrayLength() {
+        assertThat(c.take(10, array), equalTo(new Integer[]{1, 4, 9, 16, 25, 36, 49, 1, 4, 9}));
+    }
+
+    @Test
+    public void canTakeNone() {
+        assertThat(c.take(0, array), equalTo(new Integer[]{}));
+    }
+
+    @Test
+    public void canTakeNFromSingletonArray() {
+        assertThat(c.take(1, new Integer[]{1}), equalTo(new Integer[]{1}));
+    }
+
+    @Test
+    public void canTakeNegativeN() {
+        assertThat(c.take(-3, array), equalTo(new Integer[]{25, 36, 49}));
+    }
+
+    @Test
+    public void canTakeNegativeNGreaterThanArrayLength() {
+        assertThat(c.take(-10, array), equalTo(new Integer[]{25, 36, 49, 1, 4, 9, 16, 25, 36, 49}));
+    }
+}
