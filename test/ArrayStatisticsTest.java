51df63be28e6f3e202ebddd8e5eba6ef30b18ceb
Rob <robmoore121@gmail.com>
2017-10-22 13:46:38 +0100

    refactor stats test

--- test/ArrayStatisticsTest.java
+++ test/ArrayStatisticsTest.java
@@ -6,24 +6,26 @@ import static org.junit.Assert.assertThat;
 
 public class ArrayStatisticsTest {
     private Conveniencer c = new Conveniencer();
+    private Double[] example = {3.0, 1.0, 4.0, 5.0, 15.0, 7.0, 11.0, 12.0};
+
 
     @Test
     public void canComputeMean() {
-        assertThat(c.mean.one(new Double[]{3.0, 1.0, 4.0, 5.0, 15.0, 7.0, 11.0, 12.0}), equalTo(7.25));
+        assertThat(c.mean.one(example), equalTo(7.25));
     }
 
     @Test
     public void canComputeVariance() {
-        assertThat(c.variance.one(new Double[]{3.0, 1.0, 4.0, 5.0, 15.0, 7.0, 11.0, 12.0}), equalTo(21.1875));
+        assertThat(c.variance.one(example), equalTo(21.1875));
     }
 
     @Test
     public void canComputeStdDev() {
-        assertEquals(4.602988, c.stdDev.one(new Double[]{3.0, 1.0, 4.0, 5.0, 15.0, 7.0, 11.0, 12.0}), 0.01);
+        assertEquals(4.602988, c.stdDev.one(example), 0.01);
     }
 
     @Test
     public void canComputeSkewness() {
-        assertEquals(0.3316435, c.skewness.one(new Double[]{3.0, 1.0, 4.0, 5.0, 15.0, 7.0, 11.0, 12.0}), 0.01);
+        assertEquals(0.3316435, c.skewness.one(example), 0.01);
     }
 }
